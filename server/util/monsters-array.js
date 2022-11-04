3b50b7eb9f20af1ec099494beed78a0a5834f2ef
Hadasss <hadas.gadish@gmail.com>
2022-11-04 14:38:53 -0700

    added game flow chart

---
+++ server/util/monsters-array.js
@@ -0,0 +1,48 @@
+// health
+// armorClass
+// name
+// damage
+export const monsters = [
+  {
+    name: "Goblin",
+    health: 10,
+    armorClass: 10,
+    damage: 4,
+  },
+  {
+    name: "Skeleton",
+    health: 8,
+    armorClass: 10,
+    damage: 2,
+  },
+  {
+    name: "Orc",
+    health: 12,
+    armorClass: 13,
+    damage: 5,
+  },
+  {
+    name: "Troll",
+    health: 20,
+    armorClass: 13,
+    damage: 8,
+  },
+  {
+    name: "Kobold",
+    health: 10,
+    armorClass: 10,
+    damage: 5,
+  },
+  {
+    name: "Giant Spider",
+    health: 15,
+    armorClass: 12,
+    damage: 9,
+  },
+  {
+    name: "Giant Rat",
+    health: 8,
+    armorClass: 10,
+    damage: 3,
+  },
+];
